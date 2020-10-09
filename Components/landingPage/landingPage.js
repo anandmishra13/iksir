06970c1b43b44281ab1a1bd671900d3d4752149f
anand <kumaranandmihra2001@gmail.com>
2020-10-09 20:27:02 +0530

    landing page

--- Components/landingPage/landingPage.js
+++ Components/landingPage/landingPage.js
@@ -1,60 +1,25 @@
 $(function(){
-    $('.slider').slick({
-        // dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  arrows: true,
-  // slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-      }
-    },
-    {
-      breakpoint: 800,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-
-  ]
-    });
-});
-
-$(function(){
-  $('.slider2').slick({
+  $('.slider').slick({
+      // dots: true,
 infinite: false,
 speed: 300,
-slidesToShow: 2,
-slidesToScroll: 2,
+slidesToShow: 4,
 arrows: true,
+// slidesToScroll: 1,
 responsive: [
   {
     breakpoint: 1024,
     settings: {
-      slidesToShow: 2,
-      slidesToScroll: 2,
+      slidesToShow: 3,
+      slidesToScroll: 3,
       infinite: true,
     }
   },
   {
-    breakpoint: 600,
+    breakpoint: 800,
     settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToShow: 2,
+      slidesToScroll: 2
     }
   },
   {
@@ -67,4 +32,43 @@ responsive: [
 
 ]
   });
+});
+
+$(function(){
+$('.slider2').slick({
+    infinite: false,
+    speed: 300,
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+          slidesToScroll: 2,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          dots: true,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          dots: true,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
+});
 });
